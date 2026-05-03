0d3928d68a5061410ca64108235fe40e83fc3325
bibek2042 <bibek2042@gmail.com>
2026-05-03 07:21:10 +0545

    push

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
 const express = require('express')
+const connectToDatabase = require('./database')
 const app = express()
+
+
+connectToDatabase()
+
 
 
 app.get("/",(req,res)=>{
@@ -17,6 +23,11 @@ app.get("/about",(req,res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Nodejs project has started at port 3000")
 })
+
+
+
+//mongodb+srv://Bibekchaudhary:<db_password>@cluster0.edebov8.mongodb.net/?appName=Cluster0
+//mongodb+srv://Bibekchaudhary:<db_password>@cluster0.edebov8.mongodb.net/?appName=Cluster0
